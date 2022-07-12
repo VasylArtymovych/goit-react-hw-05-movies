174d8c1fa63d1d84cbd5f1e27afc4c7675df2392
VasylArtymovych <art777vasss@gmail.com>
2022-07-12 20:36:30 +0300

    add header

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,9 @@
-import {
-  fetchTrending,
-  fetchMovieCredits,
-  fetchMovieReviews,
-} from 'components/ServerAPI/ServerApi';
-import { useState } from 'react';
+import { fetchTrending } from 'components/ServerAPI/ServerApi';
+// import { useState } from 'react';
 
 export default function HomePage() {
   // const [movies, setMovies] = useState([]);
-  fetchMovieReviews(560057).then(console.log);
+  fetchTrending().then(console.log);
 
   return <div>Home</div>;
 }
